4acc9bfe296de65006fdfed1e8b94ae145c11de2
Falko <falko.becker@informatik.hu-berlin.de>
2019-02-07 22:01:00 +0100

    Distance cam

--- src/VulkanTutorial.cpp
+++ src/VulkanTutorial.cpp
@@ -1289,7 +1289,8 @@ private:
         auto rotation = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
         ubo.model = rotation;
         ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-        ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float) swapChainExtent.height, 0.1f,
+        // Entfernung der Kamera zum Objekt
+        ubo.proj = glm::perspective(glm::radians(60.0f), swapChainExtent.width / (float) swapChainExtent.height, 0.1f,
                                     10.0f);
         ubo.proj[1][1] *= -1;
 
